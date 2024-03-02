0deb24d4dc12d8d48843024bfe7bf0e1f415cc7b
Julian Lasso <julianlasso1133109@correo.itm.edu.co>
2024-03-01 23:25:42 -0500

    se agrega interface para el manejo de los modelos

--- DataBase/Models/Contact.cs
+++ DataBase/Models/Contact.cs
@@ -1,9 +1,9 @@
 namespace HolaMundo.DataBase.Models;
 
-public class Contact
+public class Contact : IModel
 {
-    public Guid Id { get; set; }
-    public required String Name { get; set; }
-    public required String Email { get; set; }
-    public required String Phone { get; set; }
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public String Name { get; set; } = String.Empty;
+    public String Email { get; set; } = String.Empty;
+    public String Phone { get; set; } = String.Empty;
 }
---
+++ DataBase/Models/ModelInterface.cs
@@ -0,0 +1,6 @@
+namespace HolaMundo.DataBase.Models;
+
+public interface IModel
+{
+    public Guid Id { get; set; }
+}
